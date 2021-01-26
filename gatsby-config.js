5697fa698c0080d6aad02bc235e8bc06397c12be
maet98 <maletavaeras@gmail.com>
2021-01-26 15:51:53 -0400

    Home page changes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,38 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+    siteMetadata: {
+        title: "Miguel's blog",
+    },
+  plugins: [
+    {
+        resolve: `gatsby-transformer-remark`,
+        options: {
+          plugins: [
+            {
+              resolve: `gatsby-remark-highlight-code`,
+              options: {
+                terminal: 'ubuntu'
+              }
+            },
+          ],
+        },
+      },
+      `gatsby-transformer-sharp`, 
+      `gatsby-plugin-sharp`,
+      `gatsby-plugin-fontawesome-css`,
+      {
+          resolve: `gatsby-source-filesystem`,
+          options: {
+            name: `src`,
+            path: `${__dirname}/src/`,
+          }
+      },
+      `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+  }],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,85 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
+import { css } from "@emotion/react"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/Layout"
+import styles from "./index.module.css"
+import perfil from "../images/perfil.jpg"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  console.log(data)
+  return (
+    <Layout>
+      <div>
+          <div className={styles.about}>
+            <h1
+              css={css`
+                display: inline-block;
+                border-bottom: 1px solid;
+              `}
+            >
+                Miguel' Blog
+            </h1>
+            <div className={styles.perfil}>
+                <img src={perfil} />
+            </div>
+            <p>
+                Soy un estudiante de ingenieria de software, entusiasta de linux, fullstack developer. Tambien me gusta el manga, el cine, libros, la musica.
+            </p>
+            <div className={styles.footer}>
+                <h4>Miguel Estevez</h4>
+                <h5>Republica Dominicana</h5>
+            </div>
+          </div>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link to={node.fields.slug}
+                css={css`
+                    text-decoration: none;
+                    color:inherit;
+                `}
+            >
+                <h3
+                  css={css`
+                    margin-bottom: ${rhythm(1 / 4)};
+                  `}
+                >
+                  {node.frontmatter.title}{" "}
+                  <span
+                    css={css`
+                      color: #bbb;
+                    `}
+                  >
+                    — {node.frontmatter.date}
+                  </span>
+                </h3>
+                <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
